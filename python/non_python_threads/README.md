746a7320be6388eed9320d5e64848bd2079c1c38
kaihsun <kaihsun@anyscale.com>
2025-02-14 21:54:20 +0000

    Add PyEval_SaveThread / PyEval_RestoreThread
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/non_python_threads/README.md
+++ python/non_python_threads/README.md
@@ -2,9 +2,11 @@
 
 * https://docs.python.org/3/c-api/init.html#non-python-created-threads
 
+## Example 1
+
 ```sh
-g++ main.cc -I/usr/include/python3.8 -lpython3.8 -lpthread -o main
-./main
+g++ example1.cc -I/usr/include/python3.9 -lpython3.9 -lpthread -o example1
+./example1
 
 # 使用 py-spy 查看，包含兩個 threads，由於 pthread_t cpp_thread 沒有執行 Python code，
 # 因此不會被 Python interpreter 管理，所以不會出現在 py-spy 的結果中。
@@ -18,4 +20,11 @@ sudo env "PATH=$PATH" py-spy dump -p 389596
 
 # Thread 389596 (idle)
 # Thread 389597 (idle)
+
+# [Example stdout]:
+# Hello from the long running cpp thread!
+# Hello from the long running python thread!
+# Hello from the long running cpp thread!
+# Hello from the long running python thread!
+# ...
 ```
--- python/non_python_threads/example1.cc
+++ python/non_python_threads/example1.cc
@@ -11,7 +11,7 @@ void* py_long_running_thread(void* arg) {
     // 這個線程會持續運行，每隔 5 秒執行一次 Python 指令
     while (1) {
         // 例如，使用 PyRun_SimpleString 呼叫 Python 代碼
-        PyRun_SimpleString("print('Hello from the long running thread!')");
+        PyRun_SimpleString("print('Hello from the long running python thread!')");
         sleep(5);
     }
 
@@ -22,7 +22,7 @@ void* py_long_running_thread(void* arg) {
 
 void* cpp_long_running_thread(void* arg) {
     while (1) {
-        printf("Hello from the long running thread2!\n");
+        printf("Hello from the long running cpp thread!\n");
         sleep(5);
     }
     return NULL;
@@ -32,8 +32,8 @@ int main(int argc, char *argv[]) {
     // 初始化 Python 解釋器
     Py_Initialize();
 
-    // 如果使用多線程，建議初始化線程支援
-    PyEval_InitThreads();
+    // 釋放 GIL，允許其他線程運行
+    PyThreadState *_save = PyEval_SaveThread();
 
     pthread_t py_thread;
     if (pthread_create(&py_thread, NULL, py_long_running_thread, NULL) != 0) {
@@ -49,6 +49,9 @@ int main(int argc, char *argv[]) {
 
     pthread_join(py_thread, NULL);
     pthread_join(cpp_thread, NULL);
+
+    // 如果沒有 RestoreThread 就呼叫 Finalize 會造成 segfault
+    PyEval_RestoreThread(_save);
     // 結束前清理 Python 解釋器（通常不會執行到這裡）
     Py_Finalize();
     return 0;
