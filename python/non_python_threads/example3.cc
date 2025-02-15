de84236fcd1ea952f2294b538afab6d6d9131d08
kaihsun <kaihsun@anyscale.com>
2025-02-15 08:12:24 +0000

    Use separate gstate

--- python/non_python_threads/example3.cc
+++ python/non_python_threads/example3.cc
@@ -49,13 +49,14 @@ int main(int argc, char *argv[]) {
     boost::asio::thread_pool default_pool(1);
     boost::asio::thread_pool custom_pool(1);
 
-    PyGILState_STATE gstate;
-    boost::asio::post(default_pool, [&gstate]() {
-        gstate = init_python_thread("default_pool");
+    PyGILState_STATE default_gstate;
+    PyGILState_STATE custom_gstate;
+    boost::asio::post(default_pool, [&default_gstate]() {
+        default_gstate = init_python_thread("default_pool");
     });
 
-    boost::asio::post(custom_pool, [&gstate]() {
-        gstate = init_python_thread("custom_pool");
+    boost::asio::post(custom_pool, [&custom_gstate]() {
+        custom_gstate = init_python_thread("custom_pool");
     });
 
     // 等待 10 秒，custom_pool 應該要在 sleep 10 秒結束前就執行完 init_python_thread
@@ -63,12 +64,12 @@ int main(int argc, char *argv[]) {
     printf("[%s] Sleep 10 seconds\n", get_current_time().c_str());
     sleep(10);
 
-    boost::asio::post(default_pool, [&gstate]() {
-        release_gstate(gstate, "default_pool");
+    boost::asio::post(default_pool, [&default_gstate]() {
+        release_gstate(default_gstate, "default_pool");
     });
 
-    boost::asio::post(custom_pool, [&gstate]() {
-        release_gstate(gstate, "custom_pool");
+    boost::asio::post(custom_pool, [&custom_gstate]() {
+        release_gstate(custom_gstate, "custom_pool");
     });
 
     // Release 後 py-spy 就看不到該 Python threads
