20221f82d419af0f8036ac01eaccd59bed8aa967
kaihsun <kaihsun@anyscale.com>
2025-03-19 04:04:04 +0000

    comment out subprocess.Popen.wait()
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- python/subprocess_wait.py
+++ python/subprocess_wait.py
@@ -8,5 +8,5 @@ time.sleep(1)
 
 # 如果沒有 process.wait() 的話，process 會變成 zombie process。
 # 因此 exists: True，如果有的話則會變成 False。
-process.wait()
+# process.wait()
 print(f"pid: {pid} exists: {psutil.pid_exists(pid)}")
